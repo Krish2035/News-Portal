0668751e430ae2d21ec635ababffe079bc378f97
Krish Padshala <krishpadshala2035@gmail.com>
2026-02-20 22:48:52 +0530

    create post api route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 
 dotenv.config();
 
@@ -21,8 +22,9 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 connectDB();
 
