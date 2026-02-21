e55d242126d639d6b61009b1c495a64a698b135e
Krish Padshala <krishpadshala2035@gmail.com>
2026-02-21 16:39:09 +0530

    Add post section to bottombar

--- frontend/src/components/shared/BottomNavBar.jsx
+++ frontend/src/components/shared/BottomNavBar.jsx
@@ -1,12 +1,15 @@
 import { signOutSuccess } from '@/redux/user/userSlice';
 import React from 'react'
 import { FaHome, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { IoIosCreate, IoIosDocument } from 'react-icons/io';
 import { MdSpaceDashboard } from "react-icons/md";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
 
 const BottomNavBar = () => {
     const dispatch = useDispatch()
+
+    const {currentUser} = useSelector((state) => state.user)
 
     const handleSignout = async () => {
       try {
@@ -24,13 +27,28 @@ const BottomNavBar = () => {
       } catch (error) {
         console.log(error);
       }
-    };
+    }
     return (
       <nav className="md:hidden fixed bottom-0 left-0 right-0 bg-slate-200 border-t border-gray-300 p-2 flex justify-around">
-        <Link to="/" className="flex flex-col items-center text-slate-800">
-          <FaHome size={20} />
-          <span className="text-xs">Home</span>
-        </Link>
+        {currentUser && currentUser.isAdmin && (
+          <Link
+            to="/create-post"
+            className="flex flex-col items-center text-slate-800"
+          >
+            <IoIosCreate size={20} />
+            <span className="text-xs">Create Post</span>
+          </Link>
+        )}
+
+        {currentUser && currentUser.isAdmin && (
+          <Link
+            to="/dashboard?tab=posts"
+            className="flex flex-col items-center text-slate-800"
+          >
+            <IoIosDocument size={20} />
+            <span className="text-xs">Posts</span>
+          </Link>
+        )}
 
         <Link
           to="/dashboard?tab=profile"
