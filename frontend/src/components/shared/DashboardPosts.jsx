95acca89b2175be700967f1f0d0c58775793a105
Krish Padshala <krishpadshala2035@gmail.com>
2026-02-22 10:22:13 +0530

    show user post inside admin dashboard

--- frontend/src/components/shared/DashboardPosts.jsx
+++ frontend/src/components/shared/DashboardPosts.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+// ADDED: TableHead and TableCell to the import list
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "../ui/table";
+// ADDED: Link import from react-router-dom
+import { Link } from "react-router-dom";
 
 const DashboardPosts = () => {
-  return (
-    <div>DashboardPosts</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
 
-export default DashboardPosts
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full p-3 overflow-x-auto">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table>
+            <TableCaption>A list of your published articles.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[200px]">Date Updated</TableHead>
+                <TableHead>Post Image</TableHead>
+                <TableHead>Post Title</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead>Delete</TableHead>
+                <TableHead>Edit</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            {/* FIXED: The map should be INSIDE TableBody */}
+            <TableBody className="divide-y">
+              {userPosts.map((post) => (
+                <TableRow key={post._id}>
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500 rounded-md"
+                      />
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className="font-medium text-gray-900"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{post.category}</TableCell>
+                  <TableCell>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className="font-medium text-green-600 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet!</p>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPosts;
